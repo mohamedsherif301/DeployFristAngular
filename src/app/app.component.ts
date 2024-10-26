967aa0a8cd2ee74f682e10688d20b4a0465b2636
Mohamed <myosre1122@gmail.com>
2024-10-27 00:46:56 +0300

    Prodject one Angular Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
+import { MainComponent } from './Components/main/main.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,MainComponent,FooterComponent,ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
